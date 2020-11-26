8da17c7a739c24115db536aa164d2f890185ab08
Sylvain Munaut <tnt@246tNt.com>
2020-11-26 11:36:01 +0100

    Whitespace fixes
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/pyfive_top.v
+++ src/pyfive_top.v
@@ -9,22 +9,22 @@
 `define WITH_RAM
 
 module pyfive_top (
-    // Wishbone Slave ports (WB MI A)
-    input  wire        wb_clk_i,
-    input  wire        wb_rst_i,
-    input  wire        wbs_stb_i,
-    input  wire        wbs_cyc_i,
-    input  wire        wbs_we_i,
-    input  wire  [3:0] wbs_sel_i,
-    input  wire [31:0] wbs_dat_i,
-    input  wire [31:0] wbs_adr_i,
-    output wire        wbs_ack_o,
-    output wire [31:0] wbs_dat_o,
+	// Wishbone Slave ports (WB MI A)
+	input  wire        wb_clk_i,
+	input  wire        wb_rst_i,
+	input  wire        wbs_stb_i,
+	input  wire        wbs_cyc_i,
+	input  wire        wbs_we_i,
+	input  wire  [3:0] wbs_sel_i,
+	input  wire [31:0] wbs_dat_i,
+	input  wire [31:0] wbs_adr_i,
+	output wire        wbs_ack_o,
+	output wire [31:0] wbs_dat_o,
 
-    // IOs
-    input  wire [15:0] io_in,
-    output wire [15:0] io_out,
-    output wire [15:0] io_oeb,
+	// IOs
+	input  wire [15:0] io_in,
+	output wire [15:0] io_out,
+	output wire [15:0] io_oeb,
 
 	// Constants
 	output wire zero,
